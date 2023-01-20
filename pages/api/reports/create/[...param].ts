877c7b9441c9ddd7c8dc60bff759e0e0024c2b8c
litolax <ura_rat@mail.ru>
2023-01-20 10:21:35 +0300

    Debug logs #2

--- pages/api/reports/create/[...param].ts
+++ pages/api/reports/create/[...param].ts
@@ -4,7 +4,7 @@ import IReport from "../../../../src/types/IReport";
 export default async (req: NextApiRequest, res: NextApiResponse) => {
 
     const { param } = req.query;
-
+    console.log(`param: ${param}`)
     if (!param || param.length < 1) return res.json({});
 
     const {db} = await connectToDatabase();
@@ -12,9 +12,9 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
 
     console.log(`parse: ${JSON.parse(param[0])}`);
     const report = JSON.parse(param[0]) as IReport;
-    console.log(`string: ${JSON.stringify(report)}`);
+    console.log(`stringify: ${JSON.stringify(report)}`);
 
     await collection.insertOne(report)
 
-    res.json({result: param})
+    res.json({result: report})
 }
